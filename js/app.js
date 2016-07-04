0f61701791e5f0677d24ef6601553f9d22f036c1
MattyB <mmbyers05@gmail.com>
2016-07-04 14:20:12 -0500

    Working to achieve MVP status...

--- js/app.js
+++ js/app.js
@@ -10,8 +10,11 @@ function playGame() {
   ss.postList = api.returnPosts();
   ss.answerList = api.returnAnswers();
   buildSet();
-  ss.questionIndex = 0;
-  ui.changeHeadline(ss.postList[ss.questionIndex]);
+  console.log("playGame");
+  $('#play-game').hide();
+  $('#reset-game').show();
+  updateProgressBar();
+  ui.changeHeadline(ss.postList[ss.getQIndex()]);
 
   console.log("postList:");
   console.log(ss.postList);
@@ -19,7 +22,19 @@ function playGame() {
   console.log(ss.answerList);
 }
 
+function resetGame() {
+  api.runReset();
+  $('#play-game').show();
+  $('#reset-game').hide();
+  $( "#canidateRow" ).empty(); 
+  ui.changeHeadline('Game Reset, please click Play...');
+  ss.resetGame();
+  updateProgressBar();
+  $( "#progress-slot" ).text('game reset'); 
+}
+
 function buildSet() {
+  ss.shuffle();
   var canidates = ss.getCanidates();
   var colors    = ss.getColors();
 
@@ -28,32 +43,66 @@ function buildSet() {
   canidates.forEach(function (ele, index) {
     ui.addToPage(ele, index, colors[index]);
   });  
+
+  var progress = 'Headline ' + (ss.getQIndex() + 1) + ' of 5';
+  $( "#progress-slot" ).text(progress); 
 };
 
 function answerQuestion (who) {
 
-  console.log(who);
-  console.log(ss.answerList[ss.questionIndex]);
-  //check answer...
+  var answer = ss.answerList[ss.getQIndex()];
   var img2 = "thumbsdown.png";
-  var imagePath = 'images/' + img2 ;
+  var response = "You are incorrect, I'm sorry.";
+  var imagePath = 'images/';
+  var retsultsText = "";
+
+  console.log("user choice:");
+  console.log(who);
+  console.log("correct answer:");
+  console.log(answer);
+
+  if (checkAnswer(who,answer)) {
+    img2 = "thumbsup.png";
+    response = "You are correct!!";
+    ss.bumpCorrect();
+  }
+
+  imagePath += img2 ;
   $('#correct-or-not-img').attr("src", imagePath);
+  $('#correct-or-not').text(response);
+  $('#correct-answer').text("That was " + answer);
+  $('#og-post').text(ss.postList[ss.getQIndex()]);
 
   $('#answerModal').foundation('open');
-  ss.questionIndex++; 
+  ss.bumpQIndex();
   updateProgressBar();
-  buildSet();
-  ui.changeHeadline(ss.postList[ss.questionIndex]);
+}
+
+function nextQ(){
+   if (ss.getQIndex() === 5){
+    $('#end-results').text("You got " + ss.getCorrect() + " of 5 correct.");
+    resetGame();
+    $('#resultsModal').foundation('open');
+  } else {
+    buildSet();
+    ui.changeHeadline(ss.postList[ss.getQIndex()]);
+  } 
 }
 
 function updateProgressBar () {
-  var percent = (ss.questionIndex * 20);
+  var correct = ss.getCorrect();
+  var percent = (correct * 20);
   var update = "width: ";
+
   update += percent;
   update += "%";
 
   $('.progress-meter').attr("style",update);
-  $('.progress-meter-text').text(percent + "%");
+  if (percent === 0) {
+    $('.progress-meter-text').text("");
+  } else {
+    $('.progress-meter-text').text(percent + "%");
+  }
 }
 
 function clickWelcome() {
@@ -61,3 +110,17 @@ function clickWelcome() {
 }
 
 function clearModal (){};
+
+function checkAnswer(who, answer) {
+
+ if (who === 'trump' && answer === 'Donald Trump'){
+  return true;
+ } else if (who === 'clinton' && answer === 'Hillary Clinton'){
+  return true;
+ } else if (who === 'sanders' && answer === 'Bernie Sanders'){
+  return true;
+ } else if (who === 'kanye' && answer === 'Kanye West'){
+  return true;
+ } 
+ return false;
+}
